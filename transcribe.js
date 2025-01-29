46ec6cb7d015fcf9a104d13dc7713fff35b8aa7a
kevin <kevinwon30@gmail.com>
2025-01-28 21:29:50 -0800

    dotenv

--- transcribe.js
+++ transcribe.js
@@ -1,26 +1,30 @@
-// Start by making sure the `assemblyai` package is installed.
-// If not, you can install it by running the following command:
-// npm install assemblyai
-
+import "dotenv/config";
 import { AssemblyAI } from "assemblyai";
 
 const client = new AssemblyAI({
-  apiKey: "ASSEMBLYAI_API_KEY",
+  apiKey: process.env.ASSEMBLYAI_API_KEY,
 });
 
-const FILE_URL = "/Users/kevin/Desktop/New Recording.m4a";
+const audioFile = "/Users/kevin/Desktop/New Recording.m4a";
 
-// You can also transcribe a local file by passing in a file path
-// const FILE_URL = './path/to/file.mp3';
-
-// Request parameters
-const data = {
-  audio: FILE_URL,
+const params = {
+  audio: audioFile,
+  speaker_labels: true,
 };
 
 const run = async () => {
-  const transcript = await client.transcripts.transcribe(data);
-  console.log(transcript.text);
+  const transcript = await client.transcripts.transcribe(params);
+
+  if (transcript.status === "error") {
+    console.error(`Transcription failed: ${transcript.error}`);
+    process.exit(1);
+  }
+
+  // console.log(transcript.text);
+
+  for (let utterance of transcript.utterances) {
+    console.log(`Speaker ${utterance.speaker}: ${utterance.text}`);
+  }
 };
 
 run();
